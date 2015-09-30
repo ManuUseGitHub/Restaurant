1ccc4d3602201e141a75953bcdd19b4c0d1342d4
ManuUseGitHub <manmdyalw@gmail.com>
2015-09-30 22:31:06 +0200

    Creating path translator
    allows to translate a path to a file string or to a folder to be written
    in the right way regarding the current plateform

---
+++ Restaurant/src/restaurant/models/javaUtilities/PathConverter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant.models.javaUtilities;
+
+/**
+ *
+ * @author MAZE2
+ */
+public class PathConverter {
+    public static String translate(String path){
+        String os = System.getProperty("os.name");
+        boolean isAWindows = os.matches("(?i:.*(WINDOWS).*)");
+        if(path.matches(".*/.*") && isAWindows ){   // on a windows but using '/'
+            return path.replace('/', '\\');
+        }
+        else if(path.matches(".*\\\\.*") && !isAWindows){ // on an different machin than a windows but using '\'
+            return path.replace('\\', '/');
+        }
+        //System.getProperties().list(System.out); // show all informations
+        return path; // no translation if nothing has to change
+    }
+}
